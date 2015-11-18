0f8f3eba8502d51de4d765c5fcddfcabd02c937d
falko_wiese <fwiese.wse@gmail.com>
2015-11-18 14:31:57 +0100

    NuGet package managament via build script.

--- serverConsole/fabfile.py
+++ serverConsole/fabfile.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
 from __future__ import print_function
-
 import inspect
 import logging
 import datetime
@@ -13,8 +12,7 @@ import time
 import re
 import codecs
 from urllib2 import URLError
-
-#import win32api
+# import win32api
 from fabric.api import *
 from fabric.colors import *
 from fabric.main import main
@@ -30,30 +28,44 @@ logging.basicConfig()
 # some common settings
 ms_build_path = r'C:\Windows\Microsoft.NET\Framework\v4.0.30319'
 nunit_path = r'C:\Program Files (x86)\NUnit.org\nunit-console'
+nuget_path = r'C:\Program Files (x86)\NuGet'
 
 path_to_test_lib = r'.\TestServerConsole\bin\Debug'
+server_solution = "serverConsole.sln"
+
 
 @task
 def all():
     clean()
+    update_env()
     build()
     test()
 
+
+@task
+def update_env():
+    update_packages(project_folder='serverConsole')
+
+
 @task
 def build():
-    ms_build(solution="serverConsole.sln")
+    ms_build(solution=server_solution)
+
 
 @task
 def clean():
-    ms_build(solution="serverConsole.sln", target="Clean")
+    ms_build(solution=server_solution, target="Clean")
+
 
 @task
 def test():
     nunit_test(path_to_dll=os.path.join(path_to_test_lib, "TestServerConsole"))
 
+
 def nunit_test(path_to_dll):
     nunit = os.path.join(nunit_path, 'nunit3-console.exe')
     local('"{nunit}" {path_to_dll}.dll'.format(nunit=nunit, path_to_dll=path_to_dll))
+
 
 def ms_build(solution, target='Build', more='/m'):
     ms_build = os.path.join(ms_build_path, 'MSBuild.exe')
@@ -65,6 +77,11 @@ def ms_build(solution, target='Build', more='/m'):
     print('Perform a {0}'.format(target.lower()))
     return local(build_cmd)
 
+
+def update_packages(project_folder):
+    pm_path = os.path.join(nuget_path, 'nuget.exe')
+    packages_file = os.path.join(project_folder, 'packages.config')
+    local('"{pm}" install {pf}'.format(pm=pm_path, pf=packages_file))
 
 
 # Main function call for the debugging
