e7c2806b4cf8eb027847cded10296702133e7be6
Falko Wiese <fwiese.wse@gmail.com>
2015-11-30 00:23:58 +0100

    Added property and method resolving based on UaVariable and UaMethod to the node manager.
    Unfortunately it's not working properly. :(

--- UaclServer/InternalNodeManager.cs
+++ UaclServer/InternalNodeManager.cs
@@ -1,20 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Reflection;
 using UnifiedAutomation.UaBase;
+using UnifiedAutomation.UaSchema;
 using UnifiedAutomation.UaServer;
 
 namespace UaclServer
 {
-	internal class InternalNodeManager : BaseNodeManager
-	{
-        private class SystemAddress
-        {
-            public int Address;
-            public int Offset;
-        }
-
+    internal class InternalNodeManager : BaseNodeManager
+    {
         public InternalNodeManager(ServerManager server, params string[] namespaceUris)
-			:base(server, namespaceUris)
-		{
+            : base(server, namespaceUris)
+        {
             CompanyUri = namespaceUris.Length < 1 || namespaceUris[0].Trim().Length <= 0 ?
                 "http://www.company.com" : namespaceUris[0];
 
@@ -25,68 +24,228 @@ namespace UaclServer
             TypeNamespaceIndex = AddNamespaceUri($"{CompanyUri}/{ApplicationUri}/types");
         }
 
-	    public string ApplicationUri { get; set; }
-	    public string CompanyUri { get; set; }
+        public string ApplicationUri { get; set; }
+        public string CompanyUri { get; set; }
         private ushort InstanceNamespaceIndex { get; set; }
         private ushort TypeNamespaceIndex { get; set; }
 
-	    private InternalServerManager getManager()
-	    {
-	        return (InternalServerManager) this.Server;
-	    }
+        private InternalServerManager getManager()
+        {
+            return (InternalServerManager)this.Server;
+        }
 
         public override void Startup()
-		{
-			Console.WriteLine("InternalNodeManager: Startup()");
+        {
+            Console.WriteLine("InternalNodeManager: Startup()");
 
             CreateUaServerInterface();
 
             base.Startup();
-		}
+        }
 
-	    private void CreateUaServerInterface()
-	    {
+        private void CreateUaServerInterface()
+        {
             // The root folder, named by the specific application, e.g. 'ServerConsole'.
-	        var applicationRoot = CreateObjectTypeNode(new CreateObjectSettings()
-	        {
-	            ParentNodeId = ObjectIds.ObjectsFolder,
-	            ReferenceTypeId = ReferenceTypeIds.Organizes,
-	            RequestedNodeId = new NodeId(ApplicationUri, InstanceNamespaceIndex),
-	            BrowseName = new QualifiedName(ApplicationUri, InstanceNamespaceIndex),
-	            TypeDefinitionId = ObjectTypeIds.FolderType
-	        });
+            var applicationRoot = AddNode(new CreateObjectSettings()
+            {
+                ParentNodeId = ObjectIds.ObjectsFolder,
+                ReferenceTypeId = ReferenceTypeIds.Organizes,
+                RequestedNodeId = new NodeId(ApplicationUri, InstanceNamespaceIndex),
+                BrowseName = new QualifiedName(ApplicationUri, InstanceNamespaceIndex),
+                TypeDefinitionId = ObjectTypeIds.FolderType
+            });
 
             // Here we add the registered objects. Unless, they aren't correctly annotated.
-	        foreach (var model in getManager().BusinessModel)
-	        {
-                var className = model.GetType().Name;
-                CreateObjectTypeNode(new CreateObjectSettings()
+            foreach (var model in getManager().BusinessModel)
+            {
+                var uaObject = model.GetType().GetCustomAttribute<UaObject>();
+                var uaObjectName = uaObject.Name ?? model.GetType().Name;
+                AddNode(new CreateObjectSettings()
                 {
                     ParentNodeId = applicationRoot.NodeId,
                     ReferenceTypeId = ReferenceTypeIds.Organizes,
-                    RequestedNodeId = new NodeId(className, InstanceNamespaceIndex),
-                    BrowseName = new QualifiedName(className, InstanceNamespaceIndex),
+                    RequestedNodeId = new NodeId(uaObjectName, InstanceNamespaceIndex),
+                    BrowseName = new QualifiedName(uaObjectName, InstanceNamespaceIndex),
                     TypeDefinitionId = ObjectTypeIds.BaseObjectType
                 }, model);
             }
         }
 
-        private ObjectNode CreateObjectTypeNode(CreateObjectSettings settings, object businessObject=null)
+        private class VariableNodeData
+        {
+            public object BusinessObject { get; set; }
+            public PropertyInfo Property { get; set; }
+        }
+
+        private class MethodNodeData
+        {
+            public object BusinessObject { get; set; }
+            public MethodInfo Method { get; set; }
+        }
+
+        private ObjectNode AddNode(CreateObjectSettings settings, object businessObject = null)
         {
             var node = CreateObject(Server.DefaultRequestContext, settings);
             Console.WriteLine($"Created node ... {node}.");
 
-            if (businessObject != null)
+            if (businessObject == null) return node;
+
+            foreach (var property in businessObject.GetType().GetProperties())
             {
-                       
+                var uaVariableAttribute = property.GetCustomAttribute<UaVariable>();
+                if (uaVariableAttribute == null) continue;
+
+                var uaVariableName = uaVariableAttribute.Name ?? property.Name;
+                var variableNode = CreateVariable(Server.DefaultRequestContext,
+                    new CreateVariableSettings()
+                    {
+                        ParentNodeId = node.NodeId,
+                        ReferenceTypeId = ReferenceTypeIds.HasComponent,
+                        RequestedNodeId = new NodeId(uaVariableName, InstanceNamespaceIndex),
+                        BrowseName = new QualifiedName(uaVariableName, InstanceNamespaceIndex),
+                        TypeDefinitionId = new NodeId(DataTypes.VariableNode, TypeNamespaceIndex),
+                        ValueType = TypeMapping.Instance.MapType(property.PropertyType),
+                        Value = new Variant(),
+                        AccessLevel = AccessLevels.CurrentRead | AccessLevels.CurrentWrite,
+                    });
+
+                variableNode.UserData = new VariableNodeData { BusinessObject = businessObject, Property = property };
+            }
+
+            foreach (var method in businessObject.GetType().GetMethods())
+            {
+                var uaMethodAttribute = method.GetCustomAttribute<UaMethod>();
+                if (uaMethodAttribute == null) continue;
+
+                var uaMethodName = uaMethodAttribute.Name ?? method.Name;
+                var methodNode = CreateMethod(Server.DefaultRequestContext,
+                    new CreateMethodSettings()
+                    {
+                        ParentNodeId = node.NodeId,
+                        ReferenceTypeId = ReferenceTypeIds.HasComponent,
+                        RequestedNodeId = new NodeId(uaMethodName, InstanceNamespaceIndex),
+                        BrowseName = new QualifiedName(uaMethodName, InstanceNamespaceIndex),
+                        TypeDefinitionId = new NodeId(DataTypes.MethodNode, TypeNamespaceIndex),
+                    });
+
+                methodNode.UserData = new MethodNodeData { BusinessObject = businessObject, Method = method };
             }
 
             return node;
         }
 
+ 
+        private sealed class TypeMapping
+        {
+            private static readonly TypeMapping instance = new TypeMapping();
+
+            private TypeMapping() { }
+
+            private Dictionary<Type, uint> typeMap;
+            private Dictionary<Type, uint> TypeMap
+            {
+                get
+                {
+                    if (typeMap != null) return typeMap;
+
+                    typeMap = new Dictionary<Type, uint>
+                    {
+                        [typeof(bool)] = DataTypes.Boolean,
+                        [typeof(byte)] = DataTypes.Byte,
+                        [typeof(short)] = DataTypes.Int16,
+                        [typeof(int)] = DataTypes.Int32,
+                        [typeof(long)] = DataTypes.Int64,
+                        [typeof(ushort)] = DataTypes.UInt16,
+                        [typeof(uint)] = DataTypes.UInt32,
+                        [typeof(ulong)] = DataTypes.UInt64,
+                        [typeof(float)] = DataTypes.Float,
+                        [typeof(double)] = DataTypes.Double,
+                        [typeof(string)] = DataTypes.String,
+                    };
+
+                    return typeMap;
+                }
+            }
+
+            public uint MapType(Type dataType)
+            {
+                if (!TypeMap.ContainsKey(dataType))
+                {
+                    throw new Exception($"Cannot find type {dataType.Name} in mapping table!");
+                }
+
+                return TypeMap[dataType];
+            }
+
+            public static TypeMapping Instance => instance;
+        }
+
+        protected override void Read(
+            RequestContext context,
+            TransactionHandle transaction,
+            IList<NodeAttributeOperationHandle> operationHandles,
+            IList<ReadValueId> settings)
+        {
+            foreach (NodeAttributeOperationHandle t in operationHandles)
+            {
+                // Initialize with bad status
+                DataValue dv = new DataValue(new StatusCode(StatusCodes.BadNodeIdUnknown));
+                // the data passed to CreateVariable is returned as the UserData in the handle.
+                /*
+                                SystemAddress address = operationHandles[ii].NodeHandle.UserData as SystemAddress;
+                                if (address != null)
+                                {
+                                    // read the data from the underlying system.
+                                    object value = m_system.Read(address.Address, address.Offset);
+                                    if (value != null)
+                                    {
+                                        dv = new DataValue(new Variant(value, null), DateTime.UtcNow);
+                                    }
+                                }
+                */
+                // return the data to the caller.
+                ((ReadCompleteEventHandler)transaction.Callback)(
+                    t,
+                    transaction.CallbackData,
+                    dv,
+                    false);
+            }
+        }
+
+        protected override void Write(
+            RequestContext context,
+            TransactionHandle transaction,
+            IList<NodeAttributeOperationHandle> operationHandles,
+            IList<WriteValue> settings)
+        {
+            foreach (NodeAttributeOperationHandle t in operationHandles)
+            {
+                // initialize with bad status
+                StatusCode error = StatusCodes.BadNodeIdUnknown;
+                // the data passed to CreateVariable is returned as the UserData in the handle.
+                /*
+                                SystemAddress address = operationHandles[ii].NodeHandle.UserData as SystemAddress;
+                                if (address != null)
+                                {
+                                    error = StatusCodes.Good;
+                                    if (!m_system.Write(address.Address, address.Offset, settings[ii].Value.Value))
+                                    {
+                                        error = StatusCodes.BadUserAccessDenied;
+                                    }
+                                }
+                */
+                // return the data to the caller.
+                ((WriteCompleteEventHandler)transaction.Callback)(
+                    t,
+                    transaction.CallbackData,
+                    error,
+                    false);
+            }
+        }
+
         public override void Shutdown()
-		{
-			base.Shutdown();
+        {
+            base.Shutdown();
             Console.WriteLine(@"InternalNodeManager: Shutdown()");
         }
     }
