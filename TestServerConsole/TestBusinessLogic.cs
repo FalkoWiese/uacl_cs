c81e6f6ec83734f5160db9c9a55bda1426960b86
Falko Wiese <fwiese.wse@gmail.com>
2015-12-08 01:43:35 +0100

    Unit test extended by a new method test of class BusinessLogic.

--- TestServerConsole/TestBusinessLogic.cs
+++ TestServerConsole/TestBusinessLogic.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Web.Script.Serialization;
 using NUnit.Framework;
 
 
@@ -8,14 +10,29 @@ namespace ServerConsole
     public class TestBusinessLogic
     {
         [Test]
-        public void testCalculate()
+        public void CallCalculate()
         {
             BusinessLogic logic = new BusinessLogic();
             Assert.IsNotNull(logic);
-            String name = "BO Logic";
-            int id = 1;
-            var result = logic.CalculateJob(name, id);
-            Assert.IsTrue(result);
+            var jobName = "Job";
+            for (var state = 0; state < 10; state++)
+            {
+                var result = logic.CalculateJob(jobName, state);
+                if(state <= 3) Assert.IsTrue(result);
+                else Assert.IsFalse(result);
+            }
         }
+
+        [Test]
+        public void CallJobStates()
+        {
+            BusinessLogic logic = new BusinessLogic();
+            Assert.IsNotNull(logic);
+            var result = logic.States();
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length > 0);
+        }
+
+
     }
 }
