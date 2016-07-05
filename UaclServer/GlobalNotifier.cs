1c4454014c72be396310e7b729794a9a8fb7e0a2
Falko Wiese <fwiese.wse@gmail.com>
2016-07-05 10:56:48 +0200

    Name from class corrected.

---
+++ UaclServer/GlobalNotifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UaclServer
+{
+    public static class GlobalNotifier
+    {
+        public static event Action<string, string,object> LocalDataChangeEvent;
+
+        public static void FireLdcEvent(string path, string variable, object value)
+        {
+            LocalDataChangeEvent?.Invoke(path, variable, value);
+        }
+    }
+}
