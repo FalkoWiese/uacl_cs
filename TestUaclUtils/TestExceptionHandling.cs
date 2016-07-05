1faf0467ee54f87c9f32f89be36b30fc30752b2c
Falko Wiese <fwiese.wse@gmail.com>
2016-07-05 13:03:57 +0200

    Stopwatch test.

--- TestUaclUtils/TestExceptionHandling.cs
+++ TestUaclUtils/TestExceptionHandling.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Threading;
 using NUnit.Framework;
+using NUnit.Framework.Compatibility;
 using UaclUtils;
+using Stopwatch = System.Diagnostics.Stopwatch;
 
 namespace TestUaclUtils
 {
     [TestFixture]
     public class TestExceptionHandling
     {
+        [Test]
+        public void StopwatchHandling()
+        {
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine($"sw started ... {sw.Elapsed.TotalSeconds}");
+            Thread.Sleep(1000);
+            sw.Stop();
+            var totalSeconds = sw.Elapsed.TotalSeconds;
+            Assert.IsTrue(totalSeconds >= 1);
+            Console.WriteLine($"sw stopped ... {totalSeconds}");
+            sw.Start();
+            Thread.Sleep(1000);
+            sw.Stop();
+            totalSeconds = sw.Elapsed.TotalSeconds;
+            Assert.IsTrue(totalSeconds >= 2);
+            Console.WriteLine($"sw second time stopped ... {totalSeconds}");
+        }
+
         [Test]
         public void LogException()
         {
