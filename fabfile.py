fd3d0385830c8dffd8a3176e720d954e614e1d5b
Falko Wiese <fwiese.wse@gmail.com>
2016-09-11 20:26:47 +0200

    Refactoring.

--- fabfile.py
+++ fabfile.py
@@ -20,7 +20,6 @@ nunit_path = r'C:\Program Files (x86)\NUnit.org\nunit-console'
 nuget_path = r'C:\Program Files (x86)\NuGet'
 
 # project path settings
-# 'MultiClientConsole', 
 impl_projects = ['MultiClientConsole', 'ServerConsole', 'OfficeConsole', 'UaclServer', 'UaclClient', 'UaclUtils']
 test_projects = ['TestServerConsole', 'TestOfficeConsole', 'TestUaclClient', 'TestUaclUtils']
 projects = impl_projects + test_projects
