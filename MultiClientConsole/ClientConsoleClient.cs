b7f9021063bb941fde1db7586ce21f9218597300
Falko Wiese <fwiese.wse@gmail.com>
2016-09-17 07:01:17 +0200

    The MultiClientConsole program is an example about a UA Server, what holds for example
    more than one UA Client.
    
    The usage of the new functionality - late binding of monitoring UA variables - is already given
    at the MultiClientConsole project.

--- MultiClientConsole/ClientConsoleClient.cs
+++ MultiClientConsole/ClientConsoleClient.cs
@@ -1,6 +1,9 @@
-﻿using UaclClient;
+﻿using System;
+using System.Collections.Generic;
+using UaclClient;
 using UaclServer;
 using UaclUtils;
+using UnifiedAutomation.UaBase;
 using UnifiedAutomation.UaClient;
 
 namespace MultiClientConsole
@@ -11,49 +14,54 @@ namespace MultiClientConsole
         private ClientConsoleClient(string ip, int port, string name) : base(ip, port, name)
         {
             MonitoringStarted = false;
-            AnnounceSessionNotConnectedHandler(DisconnectCallback);
-        }
-
-        private void DisconnectCallback(Session s, ServerConnectionStatusUpdateEventArgs args)
-        {
-            if (s.ConnectionStatus != ServerConnectionStatus.Connected)
+            AnnounceSessionNotConnectedHandler(
+                (session, args) =>
+                {
+                    if (session.ConnectionStatus != ServerConnectionStatus.Connected)
+                    {
+                        // Here, you can execute some useful stuff to handle the connection, maybe you can call s.Disconnect()
+                        // or something else. Test it, and give me maybe some feedback.
+                        Logger.Warn(
+                            $"The connection {session} is disconnected, you can find the reason maybe at the arguments ... {args}.");
+                        MonitoringStarted = false;
+                    }
+                });
+            AnnouncePostConnectionEstablishedHandler(() =>
             {
-                // Here, you can execute some useful stuff to handle the connection, maybe you can call s.Disconnect()
-                // or something else. Test it, and give me maybe some feedback.
-                Logger.Warn(
-                    $"The connection {s} is disconnected, you can find the reason maybe at the arguments ... {args}.");
-            }
+                Logger.Info("Connection to ClientConsole established ...");
+
+                if (MonitoringStarted) return;
+                Monitor(new Dictionary<string, Action<Variant>>
+                {
+                    {
+                        "BoState", strValue =>
+                        {
+                            CcBoState = strValue.ToString();
+                            Logger.Info($"Received value from {Name}.BoState ... '{CcBoState}'.");
+                        }
+                    },
+                    {
+                        "IntBoState", intValue =>
+                        {
+                            CcIntBoState = intValue.ToInt32();
+                            Logger.Info($"Received value from {Name}.IntBoState ... {CcIntBoState}.");
+                        }
+                    },
+                    {
+                        "FloatBoState", floatValue =>
+                        {
+                            CcFloatBoState = floatValue.ToFloat();
+                            Logger.Info($"Received value from {Name}.FloatBoState ... {CcFloatBoState}.");
+                        }
+                    }
+                });
+
+                MonitoringStarted = true;
+            });
         }
 
         public ClientConsoleClient() : this("localhost", 48040, "BusinessLogic")
         {
-        }
-
-        [UaMethod]
-        public void StartMonitoring()
-        {
-            if (MonitoringStarted) return;
-
-            Connect();
-            Monitor("BoState", strValue =>
-            {
-                CcBoState = strValue.ToString();
-                Logger.Info($"Received value from {Name}.BoState ... '{CcBoState}'.");
-            });
-
-            Monitor("IntBoState", intValue =>
-            {
-                CcIntBoState = intValue.ToInt32();
-                Logger.Info($"Received value from {Name}.IntBoState ... {CcIntBoState}.");
-            });
-
-            Monitor("FloatBoState", floatValue =>
-            {
-                CcFloatBoState = floatValue.ToFloat();
-                Logger.Info($"Received value from {Name}.FloatBoState ... {CcFloatBoState}.");
-            });
-
-            MonitoringStarted = true;
         }
 
         [UaVariable]
--- MultiClientConsole/ServerConsoleClient.cs
+++ MultiClientConsole/ServerConsoleClient.cs
@@ -1,6 +1,7 @@
 ﻿using UaclClient;
 using UaclServer;
 using UaclUtils;
+using UnifiedAutomation.UaClient;
 
 namespace MultiClientConsole
 {
@@ -10,24 +11,51 @@ namespace MultiClientConsole
         private ServerConsoleClient(string ip, int port, string name) : base(ip, port, name)
         {
             MonitoringStarted = false;
+            AnnounceSessionNotConnectedHandler(DisconnectCallback);
+            AnnouncePostConnectionEstablishedHandler(() =>
+            {
+                Logger.Info("Connection to ServerConsole established ...");
+                StartMonitoringImpl();
+            });
+        }
+
+        private void DisconnectCallback(Session s, ServerConnectionStatusUpdateEventArgs args)
+        {
+            if (s.ConnectionStatus != ServerConnectionStatus.Connected)
+            {
+                // Here, you can execute some useful stuff to handle the connection, maybe you can call s.Disconnect()
+                // or something else. Test it, and give me maybe some feedback.
+                Logger.Warn(
+                    $"The connection {s} is disconnected, you can find the reason maybe at the arguments ... {args}.");
+                MonitoringStarted = false;
+            }
         }
 
         public ServerConsoleClient() : this("localhost", 48030, "BusinessLogic")
         {
         }
 
-        [UaMethod]
-        public void StartMonitoring()
+        private void StartMonitoringImpl()
         {
             if (MonitoringStarted) return;
 
-            Connect();
             Monitor("BoState", v =>
             {
                 ScBoState = v.ToString();
                 Logger.Info($"Received value from {Name}.BoState ... '{ScBoState}'.");
             });
             MonitoringStarted = true;
+        }
+
+        [UaMethod]
+        public void StartMonitoring()
+        {
+            if (!Connected())
+            {
+                Connect();
+            }
+
+            StartMonitoringImpl();
         }
 
         [UaVariable]
