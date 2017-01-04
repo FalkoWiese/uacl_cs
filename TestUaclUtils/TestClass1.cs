76f809936349ac73dd340fe7cb667e5dba3491f3
falko.wiese <fwiese.wse@gmail.com>
2017-01-04 11:44:12 +0100

    Tests for the ReflectionHandler class.

---
+++ TestUaclUtils/TestClass1.cs
@@ -0,0 +1,12 @@
+﻿namespace TestUaclUtils
+{
+    public class TestClass1
+    {
+        public TestClass1(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+    }
+}
