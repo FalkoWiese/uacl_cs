534f593ea5f3c0b7cef12aa4c958b0b9f48f39b5
falko.wiese <fwiese.wse@gmail.com>
2017-01-04 11:41:46 +0100

    MultiClientHost class added.
    It encapsulates more than one client object.

---
+++ MultiClientConsole/MultiClientHost.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UaclServer;
+using UaclUtils;
+
+namespace MultiClientConsole
+{
+    [UaObject]
+    public class MultiClientHost : ServerSideUaProxy
+    {
+        public MultiClientHost()
+        {
+            Items = new List<object>();
+        }
+
+        [UaMethod]
+        public string CollectData()
+        {
+            var result = new StringBuilder("");
+            foreach (var item in Items)
+            {
+                var rf = new ReflectionHelper(item);
+
+                string itemResult;
+                try
+                {
+                    itemResult = rf.ReadProperty<string>("CcBoState", "ScBoState");
+                }
+                catch (Exception e)
+                {
+                    itemResult = e.Message;
+                }
+
+                result.AppendLine($"Result from {rf.ItemName} => {itemResult}");
+            }
+
+            return result.ToString();
+        }
+
+        [UaObjectList]
+        public List<object> Items { get; }
+    }
+}
