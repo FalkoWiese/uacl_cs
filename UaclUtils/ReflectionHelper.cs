9577958f47a18fb1e59d087d2ef76e5fa8977360
falko.wiese <fwiese.wse@gmail.com>
2017-01-04 11:43:17 +0100

    Simple .NET introspection helper class added.
    For now it encapsulates a pretty liberal reading of properties.

---
+++ UaclUtils/ReflectionHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UaclUtils
+{
+    public class ReflectionHelper
+    {
+        public ReflectionHelper(object item)
+        {
+            if (item == null) throw new Exception("The argument <item> cannot be null!");
+            Item = item;
+            ItemType = Item.GetType();
+            ItemName = ItemType.FullName;
+        }
+
+        public T ReadProperty<T>(params string[] names)
+        {
+            foreach (var name in names)
+            {
+                var description = $"{ItemName}::{name}";
+                try
+                {
+                    var propertyInfo = ItemType.GetProperty(name);
+                    if (propertyInfo == null)
+                    {
+                        continue;
+                    }
+
+                    if (!propertyInfo.CanRead)
+                    {
+                        throw new Exception($"Cannot read {description}");
+                    }
+
+                    return (T) propertyInfo.GetValue(Item);
+                }
+                catch (Exception e)
+                {
+                    ExceptionHandler.Log(e, $"Cannot read {description} without errors!");
+                    throw;
+                }
+            }
+
+            throw new Exception($"Cannot find {names} at {ItemName}.");
+        }
+
+        private object Item { get; }
+        private Type ItemType { get; }
+        public string ItemName { get; }
+    }
+}
