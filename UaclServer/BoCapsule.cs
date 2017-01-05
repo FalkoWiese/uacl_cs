694c975e6a956a341f917b201d601d5bb5a0dd05
falko.wiese <fwiese.wse@gmail.com>
2017-01-05 01:20:47 +0100

    Added some helper classes.
    BoCapsule to have an encapsulation for an arbitrary business object.
    DateTimeHelper to get the milliseconds of the current moment.

---
+++ UaclServer/BoCapsule.cs
@@ -0,0 +1,21 @@
+﻿using UnifiedAutomation.UaBase;
+
+namespace UaclServer
+{
+    public class BoCapsule
+    {
+        public BoCapsule(object model)
+        {
+            BoModel = model;
+            BoId = NodeId.Null;
+        }
+
+        public NodeId BoId { get; set; }
+        public object BoModel { get; set; }
+
+        public bool IsRegistered()
+        {
+            return BoId != NodeId.Null;
+        }
+    }
+}
---
+++ UaclUtils/DateTimeHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UaclUtils
+{
+    public class DateTimeHelper
+    {
+        public static long currentTimeMillis()
+        {
+            return DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+    }
+}
